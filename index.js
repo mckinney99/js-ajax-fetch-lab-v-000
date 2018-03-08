17c297c7a06b0c952ef3a98d2b7a699322713c8d
Eric McKinney <mckinney99@gmail.com>
2018-03-08 20:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+const token = 'YOUR_TOKEN_HERE';
+ 
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
